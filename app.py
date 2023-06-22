446e62a796a08aa99bc0cad1877aa73570b1f35d
afrancel <hi@afrancel.com>
2023-06-22 07:10:00 -0400

    update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#************** base ***************#
+
 #importar
 from flask import Flask
 from flask import render_template
@@ -5,12 +7,36 @@ from flask import render_template
 #creamos la app. Si ejecutamos este archivo, sera 'main', pero desde otro archivo será 'holamundo.py'
 app = Flask(__name__)
 
-#************** index ***************#
-#decoradores
+
+#************** Instanciamos ********#
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#************** sitio ***************#
+
 @app.route('/') #método para indicar la ruta, se ejecutara cuando llamemos la raíz
 def index(): #definimos una función llamada index
     return render_template('sitio/index.html') #indicamos la ruta
 
-#Instanciamos
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/medicos')
+def medicos():
+    return render_template('sitio/medicos.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('sitio/nosotros.html')
+
+#************** admin ***************#
+
+@app.route('/admin/') #método para indicar la ruta, se ejecutara cuando llamemos la raíz
+def admin_index(): #definimos una función llamada index
+    return render_template('/admin/index.html') #indicamos la ruta
+
+@app.route('/admin/medicos')
+def admin_medicos():
+    return render_template('/admin/medicos.html')
+
+@app.route('/admin/nosotros')
+def admin_nosotros():
+    return render_template('/admin/nosotros.html')
