bdad99e2570ac4814d09d10453a79b4603eb7525
afrancel <hi@afrancel.com>
2023-06-30 05:18:29 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #************** IMPORTAMOS ***************#
+#ojo, todas las importaciones que yo haga de la misma fuente, pueden ser separadas por coma en un linea,
+#solo que las separo para identificar de que trata cada una
+
 from flask import Flask #importamos la libreria
 from flask import render_template #importamos el lector de plantillas
 from flask import request, redirect #importamos para hacer solicitudes y redirecciones
+from flask import session #importamos las sesiones de usuarios
 from flaskext.mysql import MySQL #importamos para gestión de BD (Esta es una extensión)
 from datetime import datetime #para generar variables de tiempo y trabajar con carga de archivos
 import os #necesario para complementar la carga de imagenes
@@ -10,6 +14,9 @@ from flask import send_from_directory #para obtener informacion de la imagen
 
 #creamos la app. Si ejecutamos este archivo, sera 'main', pero desde otro archivo será 'holamundo.py'
 app = Flask(__name__)
+
+#llave secreta
+app.secret_key = "testing"
 
 #configuramos la base de datos
 mysql = MySQL()
@@ -32,7 +39,14 @@ def cargar_imagen(imagen):
 
 @app.route('/medicos')
 def medicos():
-    return render_template('sitio/medicos.html')
+
+    conexion = mysql.connect() #--> conexion inicial de la hoja
+    cursor = conexion.cursor() #--> conexion sistema/BD
+    cursor.execute("SELECT * FROM medicos;") # --> Selección con cursor la tabla / ojo revisión de nuevo comillas
+    lista_medicos = cursor.fetchall()  #--> Ejecuto la sección en la BD
+    conexion.commit()  #--> comento como siempre
+
+    return render_template('sitio/medicos.html', medicos=lista_medicos)
 
 @app.route('/nosotros')
 def nosotros():
@@ -65,16 +79,40 @@ def admin_nosotros():
 def admin_login():
     return render_template('/admin/login.html')
 
-@app.route('/admin/medicos/guardar',methods=['POST']) #ruta para el request del form de medicos
+#LOGIN: RECUPERO EL INGRESO DE DATOS AL FORM DE LOGIN
+#VERIFICO SI COINCIDE CON LOS DATOS DE ACCESO
+@app.route('/admin/login', methods=['POST'])
+def admin_login_post():
+    _user = request.form['user_info']
+    _pass = request.form['pass_info']
+    print(_user,_pass)
+
+    if _user=="admin" and _pass=="admin":
+        session['login'] = True
+        session['Usuario'] = "Francel"
+        return redirect('/admin')
+
+    return render_template('/admin/login.html')
+
+#SESIÓN: DESTRUYO EL INICIO DE SESION AL IR A ESTE LINK
+@app.route('/admin/cerrar')
+def admin_login_cerrar():
+    session.clear()
+    return redirect('/admin/login')
+
+#ruta para el request del form de medicos
+@app.route('/admin/medicos/guardar',methods=['POST'])
 def admin_medicos_guardar():
 
 #CAPTURAR DATOS DEL FORM SIN IMAGEN
+
     #creo estas variables, que tomarán la info del form de la página e indica que campo del form tomará
     _nombre = request.form['medico_nombre_form']
     _url_linkedin = request.form['medico_url_form']
     _imagen = request.files['medico_imagen_form']
 
 #SUBIR IMAGEN DEL FORM
+
     #configuración carga y muestra de imagenes
     #Para esto --> importamos from datetime import datetime
     fecha_carga = datetime.now()
@@ -85,6 +123,7 @@ def admin_medicos_guardar():
         _imagen.save("templates/imagenes/"+imagenNueva)    
 
 #INSERTAR IMAGEN DEL FORM EN BD
+
     #creo esta variable, para insertar los datos anteriores en la bd
     #01 instrucción SQL con ayuda de workbench
     sql = "INSERT INTO medicos (id, nombre, url, imagen) VALUES (NULL,%s,%s,%s);"
@@ -106,16 +145,29 @@ def admin_medicos_borrar():
 
     _medico_id_borrar = request.form['medico_id_borrar']
 
+
+#BORRAR REGISTRO DEL SERVIDOR SIN IMAGEN-->
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT imagen FROM medicos WHERE id = %s;", (_medico_id_borrar))
+    medicos = cursor.fetchall()
+    conexion.commit()
+    print(medicos)
+
+#BORRAR IMAGEN DEL SERVIDOR-->
+    #si existe esta imagen, con el click se borra la imagn directo del servidor
+    if os.path.exists("templates/imagenes/"+str(medicos[0][0])):
+        os.unlink("templates/imagenes/"+str(medicos[0][0]))
+
 #BORRAR REGISTRO DE LA BASE DE DATOS
+
 #<!-- este bloque para borrar la info -->
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("DELETE FROM medicos WHERE id = %s;", (_medico_id_borrar))
     conexion.commit()
 
-    return redirect('/admin/medicos') #--> redirecciona a la pagina de listado, es decir, medicos
-
-
+    return redirect('/admin/medicos')
 
 #************** Instanciamos ********#
 if __name__ == '__main__':
